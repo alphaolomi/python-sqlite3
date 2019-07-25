70fb272e54a01548e5deac12dabc53e075f24ae7
Alpha Olomi <alphaolomi@gmail.com>
2019-07-26 02:04:35 +0300

    add tests and module

--- README.rst
+++ README.rst
@@ -5,5 +5,3 @@ A minimal SQLite Python project
 This is the README file for the project.
 
 Using `reStructuredText <http://docutils.sourceforge.net/rst.html>`_.
-
-
--- module/main.py
+++ module/main.py
@@ -1,6 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "Alpha Olomi"
+__version__ = "0.1.0"
+__license__ = "Apache 2"
+
+from logzero import logger
+
 
 def main():
-    print('main function')
+    """ Main entry point of the app """
+    logger.info("hello world")
+
 
 if __name__ == "__main__":
+    """ This is executed when run from the command line """
     main()
